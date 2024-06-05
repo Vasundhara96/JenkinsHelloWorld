3d7eacbf286f3151a6ec07248ba9c60f3057ac80
Vasundhara96 <67625872+Vasundhara96@users.noreply.github.com>
2024-06-05 16:02:02 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld
 {
     public static void main(String []args)
     {
-        System.out.println("*****Updating the GIT*******");
+        System.out.println("*****Via webhook*******");
     }
 };
